21c139f29e41283c3822f9275e3f1f4e09c180ad
Dawid <kedzierskidawid@icloud.com>
2019-03-13 16:17:16 +0100

    some updates

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
 const mongoose = require('mongoose');
 const databaseURI = 'mongodb://localhost:27017/myapp';
 
@@ -21,13 +17,13 @@ const UserModel = mongoose.model('User', UserSchema);
 // You can read more about method in Model object in the documentation
 // https://mongoosejs.com/docs/api.html#Model
 
-// UserModel.create({ firstName: 'Dawid', lastName: 'Kędzierski'}, (err) => {
-//     if (err) {
-//         console.error(err);
-//     }
-// });
+UserModel.create({ firstName: 'Dawid', lastName: 'Kędzierski' }, (err) => {
+    if (err) {
+        console.error(err);
+    }
+});
 
-// UserModel.find({ firstName: 'Dawid'}, (err, data) => {
+// UserModel.find({ firstName: 'Dawid' }, (err, data) => {
 //     if (err) {
 //         console.error(err);
 //     }
@@ -37,7 +33,7 @@ const UserModel = mongoose.model('User', UserSchema);
 
 // const userId = '5c891cb919caba6b50f492a4';
 
-// UserModel.updateOne({ _id: userId }, { lastName: 'Kędzierski'}, (err, raw) => {
+// UserModel.updateOne({ _id: userId }, { lastName: 'Kędzierski' }, (err, raw) => {
 //     if (err) {
 //         console.error(err);
 //     }
